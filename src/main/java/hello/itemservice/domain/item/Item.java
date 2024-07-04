3c111095db331625230d6670878eab46ec10977d
master <ted3131@diquest.com>
2024-07-04 16:00:16 +0900

    commit

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,21 +12,23 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 만원 넘게 입력해주세요")
 public class Item {
 
-    @NotNull //수정 요구사항 추가
+    @NotNull(groups = UpdateCheck.class) //수정시에만 적용
     private Long id;
-    @NotBlank
-    private String itemName;
-    @NotNull
-    @Range(min = 1000, max = 1000000)
-    private Integer price;
-    @NotNull
-    //@Max(9999) //수정 요구사항 추가
-    private Integer quantity;
 
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+    private String itemName;
+
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class,
+            UpdateCheck.class})
+    private Integer price;
+
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = SaveCheck.class) //등록시에만 적용
+    private Integer quantity;
 
     public Item() {
     }
-
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
         this.price = price;
