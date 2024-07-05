4426bd9492c128de0d15934b04644187851093f5
master <ted3131@diquest.com>
2024-07-05 16:03:52 +0900

    commit

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV4.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV4.java
@@ -85,6 +85,7 @@ public class ValidationItemControllerV4 {
         itemParam.setItemName(form.getItemName());
         itemParam.setPrice(form.getPrice());
         itemParam.setQuantity(form.getQuantity());
+
         itemRepository.update(itemId, itemParam);
         return "redirect:/validation/v4/items/{itemId}";
     }
